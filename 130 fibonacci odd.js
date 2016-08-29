ee6bc612599c9508a686385be721ca3ef7022b09
Vladimir Gasic <vgashic@gmail.com>
2016-08-30 00:17:16 +0200

    Fibonacci finished

---
+++ 130 fibonacci odd.js
@@ -0,0 +1,26 @@
+/*
+Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than 10 are 1, 1, 3, and 5.
+*/
+
+function fibonacci(n) {
+  "use strict";
+
+  var res = [1, 1];
+
+  for (var i = 2; i <= n; i++) {
+    res.push(res[i - 2] + res[i - 1]);
+  }
+
+  return res;
+}
+
+function sumFibs(num) {
+  return num;
+}
+
+console.log(fibonacci(2));
+//sumFibs(4);
